96f646526b45be1a43ed9ba99ecaab8d26116f6e
Alireza Hamid <alirezahamid20@gmail.com>
2021-04-07 00:09:20 +0430

    Implement GET API for fetch planets

--- mod.ts
+++ mod.ts
@@ -1,19 +1,38 @@
-import { Application } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
+import { Application, send } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
+import api from './api.ts'
 
 const app = new Application()
 const PORT = 8000
 
-app.use((ctx) => {
-    ctx.response.body = `
-    _   _____   _____ ___ 
-    / | / /   | / ___//   |
-   /  |/ / /| | \__ \/ /| |
-  / /|  / ___ |___/ / ___ |
- /_/ |_/_/  |_/____/_/  |_|
-             
-    Nasa Mission Control                           
-    
-    `
+app.use(async(ctx, next) => {
+    await next()
+    const time = ctx.response.headers.get("X-Response-Time")
+    console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`)
+})
+
+app.use(api.routes())
+app.use(api.allowedMethods())
+
+app.use(async(ctx, next) => {
+    const start = Date.now()
+    await next()
+    const delta = Date.now() - start
+    ctx.response.headers.set("X-Response-Time", `${delta} ms`)
+})
+
+app.use(async (ctx) => {
+    const filePath = ctx.request.url.pathname
+    const fileWhiteList = [
+        "/index.html",
+        "/javascripts/scripts.js",
+        "/stylesheets/style.css",
+        "/images/favicon.png",
+    ]
+    if (fileWhiteList.includes(filePath)) {
+        await send(ctx, filePath, {
+            root: `${Deno.cwd()}/public`,
+        })
+    }
 })
 
 if (import.meta.main) {
